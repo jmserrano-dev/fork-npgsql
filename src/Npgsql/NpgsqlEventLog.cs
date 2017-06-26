32bf86cd05bcd2e5127fddd32ba3d3ee7c8b2338
Jacobo van Leeuwen <jvanleeuwen@evicertia.com>
2017-06-26 22:30:31 +0200

    Improve NpgsqlEventLog with caller class name

--- src/Npgsql/NpgsqlEventLog.cs
+++ src/Npgsql/NpgsqlEventLog.cs
@@ -14,13 +14,13 @@
 // documentation for any purpose, without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
 // and this paragraph and the following two paragraphs appear in all copies.
-// 
+//
 // IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
 // FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
 // INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
 // DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
 // THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
 // INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 // AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
@@ -145,18 +145,20 @@ namespace Npgsql
 		/// </remarks>
 		/// <param name="message">The message to write to the event log</param>
 		/// <param name="msglevel">The minimum <see cref="Npgsql.LogLevel">LogLevel</see> for which this message should be logged.</param>
-		private static void LogMsg(String message, LogLevel msglevel)
+		private static void LogMsg(string message, LogLevel msglevel, string className = null)
 		{
 			if (msglevel > level)
 			{
 				return;
 			}
 
+			var logger = className == null ? _Log : global::Common.Logging.LogManager.GetLogger(className);
+
 			switch (msglevel)
 			{
-				case LogLevel.Debug: _Log.Debug(message); break;
-				case LogLevel.Normal: _Log.Info(message); break;
-				default: _Log.Info(message); break;
+				case LogLevel.Debug: logger.Debug(message); break;
+				case LogLevel.Normal: logger.Info(message); break;
+				default: logger.Info(message); break;
 			}
 
 			if (echomessages)
@@ -188,7 +190,7 @@ namespace Npgsql
 		/// <param name="msglevel">The minimum <see cref="Npgsql.LogLevel">LogLevel</see> for which this message should be logged.</param>
 		/// <param name="Parameters">The additional parameters that shall be included into the log-message (must be compatible with the string in the resource):</param>
 		internal static void LogMsg(ResourceManager resman, string ResourceString, LogLevel msglevel,
-		                            params Object[] Parameters)
+																params Object[] Parameters)
 		{
 			if (msglevel > level)
 			{
@@ -222,7 +224,7 @@ namespace Npgsql
 				return;
 			}
 			string message = String.Format(LogResMan.GetString("Indexer_Get"), ClassName, IndexerParam);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -239,7 +241,7 @@ namespace Npgsql
 				return;
 			}
 			string message = String.Format(LogResMan.GetString("Indexer_Set"), ClassName, IndexerParam, value);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -255,7 +257,7 @@ namespace Npgsql
 				return;
 			}
 			string message = String.Format(LogResMan.GetString("Property_Get"), ClassName, PropertyName);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -272,7 +274,7 @@ namespace Npgsql
 				return;
 			}
 			string message = String.Format(LogResMan.GetString("Property_Set"), ClassName, PropertyName, value);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -288,7 +290,7 @@ namespace Npgsql
 				return;
 			}
 			string message = String.Format(LogResMan.GetString("Method_0P_Enter"), ClassName, MethodName);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -305,7 +307,7 @@ namespace Npgsql
 				return;
 			}
 			string message = String.Format(LogResMan.GetString("Method_1P_Enter"), ClassName, MethodName, MethodParameter);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -317,7 +319,7 @@ namespace Npgsql
 		/// <param name="MethodParameter1">The value of the first Argument of the Method</param>
 		/// <param name="MethodParameter2">The value of the second Argument of the Method</param>
 		internal static void LogMethodEnter(LogLevel msglevel, string ClassName, string MethodName, object MethodParameter1,
-		                                    object MethodParameter2)
+											object MethodParameter2)
 		{
 			if (msglevel > level)
 			{
@@ -325,7 +327,7 @@ namespace Npgsql
 			}
 			string message =
 				String.Format(LogResMan.GetString("Method_2P_Enter"), ClassName, MethodName, MethodParameter1, MethodParameter2);
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -338,7 +340,7 @@ namespace Npgsql
 		/// <param name="MethodParameter2">The value of the second Argument of the Method</param>
 		/// <param name="MethodParameter3">The value of the third Argument of the Method</param>
 		internal static void LogMethodEnter(LogLevel msglevel, string ClassName, string MethodName, object MethodParameter1,
-		                                    object MethodParameter2, object MethodParameter3)
+											object MethodParameter2, object MethodParameter3)
 		{
 			if (msglevel > level)
 			{
@@ -346,8 +348,8 @@ namespace Npgsql
 			}
 			string message =
 				String.Format(LogResMan.GetString("Method_3P_Enter"), ClassName, MethodName, MethodParameter1, MethodParameter2,
-							  MethodParameter3);
-			LogMsg(message, msglevel);
+								MethodParameter3);
+			LogMsg(message, msglevel, ClassName);
 		}
 
 		/// <summary>
@@ -358,7 +360,7 @@ namespace Npgsql
 		/// <param name="MethodName">The name of the Method</param>
 		/// <param name="MethodParameters">A <see cref="System.Object">Object</see>-Array with zero or more Ojects that are Arguments of the Method.</param>
 		internal static void LogMethodEnter(LogLevel msglevel, string ClassName, string MethodName,
-		                                    params object[] MethodParameters)
+											params object[] MethodParameters)
 		{
 			if (msglevel > level)
 			{
@@ -370,18 +372,18 @@ namespace Npgsql
 				case 4:
 					message =
 						String.Format(LogResMan.GetString("Method_4P_Enter"), ClassName, MethodName, MethodParameters[0],
-									  MethodParameters[1], MethodParameters[2], MethodParameters[3]);
+									MethodParameters[1], MethodParameters[2], MethodParameters[3]);
 					break;
 				case 5:
 					message =
 						String.Format(LogResMan.GetString("Method_5P_Enter"), ClassName, MethodName, MethodParameters[0],
-									  MethodParameters[1], MethodParameters[2], MethodParameters[3], MethodParameters[4]);
+									MethodParameters[1], MethodParameters[2], MethodParameters[3], MethodParameters[4]);
 					break;
 				case 6:
 					message =
 						String.Format(LogResMan.GetString("Method_6P_Enter"), ClassName, MethodName, MethodParameters[0],
-									  MethodParameters[1], MethodParameters[2], MethodParameters[3], MethodParameters[4],
-									  MethodParameters[5]);
+									MethodParameters[1], MethodParameters[2], MethodParameters[3], MethodParameters[4],
+									MethodParameters[5]);
 					break;
 				default:
 					// should always be true - but who knows ;-)
@@ -389,11 +391,11 @@ namespace Npgsql
 					{
 						message =
 							String.Format(LogResMan.GetString("Method_6P+_Enter"), ClassName, MethodName, MethodParameters[0],
-										  MethodParameters[1]);
+										MethodParameters[1]);
 					}
 					break;
 			}
-			LogMsg(message, msglevel);
+			LogMsg(message, msglevel, ClassName);
 		}
 	}
 }
